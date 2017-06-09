c59eb0830395b247fe6971a50ca74132a562f240
Balaram <babandi@cisco.com>
2017-06-09 13:59:54 +0530

    1. JWT token generation 2. Query all questions, single question, add answer.

--- QAForum/src/main/java/com/cmad/mongo/MongoServiceVerticle.java
+++ QAForum/src/main/java/com/cmad/mongo/MongoServiceVerticle.java
@@ -5,16 +5,18 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 
 public class MongoServiceVerticle extends AbstractVerticle {
+
     @Override
     public void start() throws Exception {
+        
+        String mongoHost = System.getProperty("MONGOHOST");
+        JsonObject config = new JsonObject();
+        config.put("db_name", "qaforum");
+        config.put("connection_string", "mongodb://"+mongoHost+":27017");
+        MongoClient client = MongoClient.createShared(super.vertx, config);
+
         vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.user.userid",
                 message -> {
-
-                    JsonObject config = new JsonObject();
-                    config.put("db_name", "qaforum");
-                    config.put("connection_string", "mongodb://mymongo:27017");
-                    MongoClient client = MongoClient.createShared(vertx,
-                            config);
 
                     client.find("user", new JsonObject().put("userid",
                             message.body().toString()), res -> {
@@ -36,35 +38,28 @@ public class MongoServiceVerticle extends AbstractVerticle {
         vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.user.add",
                 message -> {
 
-                    JsonObject config = new JsonObject();
-                    config.put("db_name", "qaforum");
-                    config.put("connection_string", "mongodb://mymongo:27017");
-                    MongoClient client = MongoClient.createShared(vertx,
-                            config);
                     System.out.println(
                             "To be Created User " + message.body().toString());
-                    client.insert("user",
-                            new JsonObject(message.body().toString()), res -> {
-                                if (res.succeeded()) {
-                                    message.reply(res.result());
-                                } else {
-                                    res.cause().printStackTrace();
-                                    message.reply(-1);
-                                }
+                    
+                    JsonObject userObject = new JsonObject(
+                            message.body().toString());
 
-                            });
+                    client.insert("user", userObject, res -> {
+                        if (res.succeeded()) {
+                            message.reply(userObject.getString("userId"));
+                        } else {
+                            res.cause().printStackTrace();
+                            message.reply(-1);
+                        }
+
+                    });
                 });
 
         vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.add",
                 message -> {
 
-                    JsonObject config = new JsonObject();
-                    config.put("db_name", "qaforum");
-                    config.put("connection_string", "mongodb://mymongo:27017");
-                    MongoClient client = MongoClient.createShared(vertx,
-                            config);
-                    System.out.println(
-                            "To be Created Question " + message.body().toString());
+                    System.out.println("To be Created Question "
+                            + message.body().toString());
                     client.insert("question",
                             new JsonObject(message.body().toString()), res -> {
                                 if (res.succeeded()) {
@@ -76,5 +71,91 @@ public class MongoServiceVerticle extends AbstractVerticle {
 
                             });
                 });
+        
+        vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.all",
+                message -> {
+
+                    client.find("question", new JsonObject(), res -> {
+                                if (res.succeeded()) {
+                                    if (res.result().size() != 0) {
+                                        message.reply(res.result().toString());
+                                    } else {
+                                        message.reply("");
+                                    }
+                                } else {
+                                    res.cause().printStackTrace();
+                                    message.reply("");
+                                }
+                            });
+                });
+
+        vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.answer.add",
+                message -> {
+                    JsonObject input = new JsonObject(
+                            message.body().toString());
+
+                    // Create the query and the update operation.
+                    JsonObject query = new JsonObject().put("_id",
+                            input.getString("qid"));
+                    JsonObject answer = new JsonObject()
+                            .put("answer", input.getString("answer"))
+                            .put("userid", input.getString("userid"))
+                            .put("postTime", System.currentTimeMillis());
+
+                    JsonObject update = new JsonObject().put("$addToSet",
+                            new JsonObject().put("answers", answer));
+
+                    client.updateCollection("question", query, update, res -> {
+                        if (res.succeeded()) {
+                            message.reply(201);
+                        } else {
+                            res.cause().printStackTrace();
+                            message.reply(-1);
+                        }
+
+                    });
+                });
+
+        vertx.eventBus().consumer("com.cmad.vertx.qaforum.question.get",
+                message -> {
+
+                    client.find("question", new JsonObject().put("_id",
+                            message.body().toString()), res -> {
+                                if (res.succeeded()) {
+                                    if (res.result().size() != 0) {
+                                        System.out.println(
+                                                "User exist " + res.result());
+                                        message.reply(res.result().toString());
+                                    } else {
+                                        message.reply("");
+                                    }
+                                } else {
+                                    res.cause().printStackTrace();
+                                    message.reply("");
+                                }
+                            });
+                });
+
+        vertx.eventBus().consumer("com.cmad.vertx.qaforum.login", message -> {
+
+            JsonObject userObject = new JsonObject(message.body().toString());
+
+            client.find("user", userObject, res -> {
+                if (res.succeeded()) {
+                    if (res.result().size() != 0) {
+                        System.out.println("User exist " + res.result());
+                        message.reply("Authentication Successful");
+                    } else {
+                        message.reply(
+                                "Authentication Failed, Invalid User/Password");
+                    }
+                } else {
+                    res.cause().printStackTrace();
+                    message.reply(
+                            "Authentication Failed, Invalid User/Password");
+                }
+            });
+        });
+
     }
 }
--- QAForum/src/main/java/com/cmad/vertx/QAForumMainVerticle.java
+++ QAForum/src/main/java/com/cmad/vertx/QAForumMainVerticle.java
@@ -3,9 +3,12 @@ package com.cmad.vertx;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.rabbitmq.RabbitMQClient;
+import io.vertx.rabbitmq.impl.RabbitMQClientImpl;
 
 public class QAForumMainVerticle extends AbstractVerticle {
     @Override
@@ -15,9 +18,13 @@ public class QAForumMainVerticle extends AbstractVerticle {
         vertx.deployVerticle("com.cmad.mongo.MongoServiceVerticle",
                 new DeploymentOptions().setWorker(true));
 
+        vertx.deployVerticle("com.cmad.jwt.JWTServiceVerticle",
+                new DeploymentOptions().setWorker(true));
+
         router.route("/static/*").handler(StaticHandler.create("web"));
 
         router.get("/qaforum/user/:userid").handler(rctx -> {
+
             vertx.eventBus().send("com.cmad.vertx.qaforum.question.user.userid",
                     rctx.request().getParam("userid"), res -> {
                         System.out.println("Result for Query by User ID "
@@ -54,9 +61,33 @@ public class QAForumMainVerticle extends AbstractVerticle {
 
         });
 
+        router.route("/qaforum/ticket").handler(BodyHandler.create());
+        router.post("/qaforum/ticket").handler(rctx -> {
+            vertx.eventBus().send("com.cmad.vertx.qaforum.login",
+                    rctx.getBodyAsJson().encodePrettily(), res -> {
+
+                        if (res.result().body().toString().contains("Failed")) {
+                            rctx.response().setStatusCode(401).end();
+                        } else {
+
+                            vertx.eventBus().send(
+                                    "com.cmad.vertx.qaforum.ticket",
+                                    rctx.getBodyAsJson().getString("userid"),
+                                    response -> {
+                                        rctx.response().setStatusCode(201)
+                                                .putHeader("Content-Type",
+                                                        "application/json")
+                                                .end(response.result().body()
+                                                        .toString());
+                                    });
+                        }
+                    });
+
+        });
+
         router.route("/qaforum/question").handler(BodyHandler.create());
         router.post("/qaforum/question").handler(rctx -> {
-            vertx.eventBus().send("com.cmad.vertx.qaforum.question.question.add",
+            vertx.eventBus().send("com.cmad.vertx.qaforum.question.add",
                     rctx.getBodyAsJson().encodePrettily(), res -> {
 
                         if (res.result().body().toString().equals("-1")) {
@@ -72,6 +103,57 @@ public class QAForumMainVerticle extends AbstractVerticle {
                         }
                     });
 
+        });
+
+        router.route("/qaforum/question/:qid/answer")
+                .handler(BodyHandler.create());
+        router.post("/qaforum/question/:qid/answer").handler(rctx -> {
+            vertx.eventBus().send("com.cmad.vertx.qaforum.question.answer.add",
+                    rctx.getBodyAsJson().put("qid",
+                            rctx.request().getParam("qid")),
+                    res -> {
+
+                        if (res.result().body().toString().equals("-1")) {
+                            rctx.response().setStatusCode(500).end();
+                        } else {
+                            rctx.response().setStatusCode(201)
+                                    .putHeader("Content-Type",
+                                            "application/json")
+                                    .putHeader("Location", "qaforum/question/"
+                                            + res.result().body().toString())
+                                    .end();
+                        }
+                    });
+
+        });
+
+        router.get("/qaforum/question/:qid").handler(rctx -> {
+
+            vertx.eventBus().send("com.cmad.vertx.qaforum.question.get",
+                    rctx.request().getParam("qid"), res -> {
+                        System.out.println("Result for Query by Question ID "
+                                + res.result().body());
+
+                        if (res.result().body().toString().isEmpty()) {
+                            rctx.response().setStatusCode(404).end();
+                        } else {
+                            rctx.response().setStatusCode(200)
+                                    .putHeader("Content-Type",
+                                            "application/json")
+                                    .end(res.result().body().toString());
+                        }
+                    });
+        });
+
+        router.get("/qaforum/question").handler(rctx -> {
+
+            vertx.eventBus().send("com.cmad.vertx.qaforum.question.all", "",
+                    res -> {
+
+                        rctx.response().setStatusCode(200)
+                                .putHeader("Content-Type", "application/json")
+                                .end(res.result().body().toString());
+                    });
         });
 
         vertx.createHttpServer().requestHandler(router::accept)
